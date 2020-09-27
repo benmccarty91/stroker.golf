02c0f26bf208510c5c8a07e047bd71ec4fe1c221
Ben McCarty <benmccarty91@gmail.com>
2020-09-27 17:56:01 -0600

    testing api only CI

--- API/functions/src/User/userController.ts
+++ API/functions/src/User/userController.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('in get users controller! :P');
+  res.send('in get users controller! let\'s try again');
 })
 
 module.exports = router;
