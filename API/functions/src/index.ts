409282296d9ccbbc6fd8eb397514ea23f2d5e519
Ben McCarty <benmccarty91@gmail.com>
2020-09-27 17:03:45 -0600

    updated api to use express router for file organization

--- API/functions/src/index.ts
+++ API/functions/src/index.ts
@@ -1,9 +1,9 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+const app = express();
+
+app.use('/user', require('./User/userController'));
+
+export const api = functions.https.onRequest(app);
