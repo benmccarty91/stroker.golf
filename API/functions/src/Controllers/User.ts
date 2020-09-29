631723ed17d9028b6d1c9bc152dd9ffeb3217467
Ben McCarty <benmccarty91@gmail.com>
2020-09-29 06:44:21 -0600

    test api change

--- API/functions/src/Controllers/User.ts
+++ API/functions/src/Controllers/User.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('in get users controller! let\'s try again');
+  res.send('hello, paul.');
 })
 
 module.exports = router;
