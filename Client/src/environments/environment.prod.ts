ac16f688a615087cfc1a75d7e1c1f3bae6a27b52
Ben McCarty <benmccarty91@gmail.com>
2020-10-04 17:19:39 -0600

    adding firebase to prod env

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBPNlKPsHyPsTicxoUWbyC4hfH-_gJ28r4',
+    authDomain: 'stroker-af4d5.firebaseapp.com',
+    databaseURL: 'https://stroker-af4d5.firebaseio.com',
+    projectId: 'stroker-af4d5',
+    storageBucket: 'stroker-af4d5.appspot.com',
+    messagingSenderId: '1061788771221',
+    appId: '1:1061788771221:web:ec541e4e8f4c113ecb8c9c',
+    measurementId: 'G-FQJ9W378JC'
+  }
 };
