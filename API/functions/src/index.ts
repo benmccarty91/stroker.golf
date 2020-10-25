eaac85c6503876a83b992a3307f8486e3670134d
Ben McCarty <benmccarty91@gmail.com>
2020-10-25 16:07:40 -0600

    adding cors policy

--- API/functions/src/index.ts
+++ API/functions/src/index.ts
@@ -1,8 +1,11 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
+import * as cors from 'cors';
 
 
 const app = express();
+
+app.use(cors({ origin: true }))
 
 app.use('/user', require('./Controllers/User'));
 app.use('/test', require('./Controllers/Test'));
