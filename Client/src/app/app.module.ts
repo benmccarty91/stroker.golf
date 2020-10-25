18457736daf00209412377bf01fcf3f4773c440f
Ben McCarty <benmccarty91@gmail.com>
2020-10-25 13:17:30 -0600

    pwa not working

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NavModule } from 'src/components/nav/nav.module';
 import { MaterialModule } from './shared/material.module';
 import { HeaderModule } from 'src/components/header/header.module';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserAnimationsModule,
     NavModule,
     HeaderModule,
-    MaterialModule
+    MaterialModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: true })
   ],
   providers: [AngularFireAuth],
   bootstrap: [AppComponent]
