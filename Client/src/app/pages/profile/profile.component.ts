bb033c641be030c8faf2141feba9130acc47bd85
Ben McCarty <benmccarty91@gmail.com>
2021-02-27 16:03:31 -0700

    can now review scores that failed to upload

--- Client/src/app/pages/profile/profile.component.ts
+++ Client/src/app/pages/profile/profile.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageMap } from '@ngx-pwa/local-storage';
 import { CONSTS } from 'src/assets/CONSTS';
+import { GolfCourse } from 'src/models/GolfCourse';
+import { Score } from 'src/models/Score';
 import { StrokerUser } from 'src/models/StrokerUser';
 import { PubSubService } from 'src/services/PubSubService';
+import { ScoreService } from 'src/services/ScoreService';
 import { UserService } from 'src/services/UserService';
 
 @Component({
@@ -14,12 +18,15 @@ export class ProfileComponent implements OnInit {
 
   public loading: Boolean = true;
   public user: StrokerUser;
+  public scores: Score[] = null;
 
   constructor(
     private userService: UserService,
     private pubSub: PubSubService,
     private consts: CONSTS,
-    private router: Router
+    private router: Router,
+    private localStorage: StorageMap,
+    private scoreService: ScoreService
   ) { }
 
   ngOnInit(): void {
@@ -28,10 +35,34 @@ export class ProfileComponent implements OnInit {
       this.loading = false;
       this.pubSub.$pub(this.consts.EVENTS.PAGE_LOAD_COMPLETE);
     });
+
+    this.localStorage.get<Score[]>(this.consts.APP_DATA.SCORE_SUBMISSIONS).subscribe((scores: Score[]) => {
+      this.scores = scores;
+    })
   }
 
   public async editFabClicked(): Promise<void> {
     await this.router.navigateByUrl('/profile/edit')
   }
+
+  public upload(score: Score): void {
+    this.scoreService.postScore(score).subscribe(
+      () => {
+        this.removeScoreFromStorage(score);
+      },
+      (err) => { alert('something went wrong') }
+    )
+  }
+
+  public delete(score: Score): void {
+    this.removeScoreFromStorage(score);
+  }
+
+  private removeScoreFromStorage(score: Score): void {
+    const index = this.scores.indexOf(score);
+    this.scores.splice(index, 1);
+    this.localStorage.set(this.consts.APP_DATA.SCORE_SUBMISSIONS, this.scores).subscribe(() => { });
+  }
+
 
 }
--- Client/src/app/shared/components/header/header.component.ts
+++ Client/src/app/shared/components/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageMap } from '@ngx-pwa/local-storage';
 import { CONSTS } from 'src/assets/CONSTS';
-import { PubSubService } from 'src/services/PubSubService';
+import { Score } from 'src/models/Score';
 import { UserService } from 'src/services/UserService';
 
 @Component({
@@ -13,12 +14,13 @@ export class HeaderComponent implements OnInit {
 
   public opened: boolean;
   public loggedIn: boolean = false;
+  public profileBadgeIcon: number = 0;
 
   @Output() toggleSidenav = new EventEmitter<void>();
   constructor(
     private router: Router,
     private userService: UserService,
-    private pubSub: PubSubService,
+    private localStorage: StorageMap,
     private consts: CONSTS
   ) { }
 
@@ -26,6 +28,13 @@ export class HeaderComponent implements OnInit {
     this.userService.isLoggedIn().subscribe(x => {
       this.loggedIn = x;
     });
+    this.localStorage.watch<Score[]>(this.consts.APP_DATA.SCORE_SUBMISSIONS).subscribe((res: Score[]) => {
+      if (res) {
+        this.profileBadgeIcon = res.length;
+      } else {
+        this.profileBadgeIcon = 0;
+      }
+    })
   }
 
   profileClicked(): void {
