53d3498ca959f8d619859ed427a6234ee6525ad2
Ben McCarty <benmccarty91@gmail.com>
2021-02-26 20:31:25 -0700

    api changes

--- API/functions/src/Controllers/User.ts
+++ API/functions/src/Controllers/User.ts
@@ -1,4 +1,4 @@
-import {StatusCodes} from 'http-status-codes';
+import { StatusCodes } from 'http-status-codes';
 
 import * as express from 'express';
 import * as admin from 'firebase-admin';
@@ -30,11 +30,37 @@ router.post('/register', async (req: any, res) => {
   }
 });
 
-router.get('/profile', async(req: any, res) => { // get full profile data
-  return res.status(StatusCodes.OK).send();
+router.get('/profile', async (req: any, res) => { // get full profile data
+  const userId = req.user.uid;
+
+  const userQuery = await userCollection.doc(userId).get();
+  const user = userQuery.data() as StrokerUser;
+  return res.status(StatusCodes.OK).send(user);
 });
 
-router.put('/profile', async(req: any, res) => { // update profile data
+router.put('/profile', async (req: any, res) => { // update profile data
+  const userId = req.user.uid;
+  const updatedUser = req.body as StrokerUser;
+
+  if (userId !== updatedUser.id) {
+    return res.status(StatusCodes.FORBIDDEN).send();
+  }
+
+  if (!updatedUser.id || !updatedUser.displayName || !updatedUser.email) {
+    return res.status(StatusCodes.BAD_REQUEST).send();
+  }
+
+  if (!updatedUser.photoUrl) {
+    updatedUser.photoUrl = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png';
+  }
+
+  const userRef = userCollection.doc(userId);
+  await userRef.update({
+    "displayName": updatedUser.displayName,
+    "email": updatedUser.email,
+    "photoUrl": updatedUser.photoUrl
+  })
+
   return res.status(StatusCodes.OK).send();
 });
 
